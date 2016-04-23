0e22989c4c35ccd8b9a0afdc729438c562410268
abc <abc@abc-VirtualBox.(none)>
2016-04-23 21:04:38 +0800

    fix SJF

--- sjf.c
+++ sjf.c
@@ -119,6 +119,7 @@ int main()
 
 		int time = decideTime(scheduler, N, nextforfork, Index, clock);
 		char buffer[MAX_BUF] = {0};	
+		sprintf(buffer, "%d", time);
 		write(scheduler[Index].fd[1], buffer, MAX_BUF);
 		struct sched_param paramforchild;
 		paramforchild.sched_priority = 99;
